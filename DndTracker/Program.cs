3e64f349e222eafcdfebc657af889f088b8bf9f2
jimmyjawnz <jamesprovee@gmail.com>
2025-07-18 14:10:56 -0600

    Made run on port 8888 for release.

--- DndTracker/Program.cs
+++ DndTracker/Program.cs
@@ -60,17 +60,16 @@ using (var serviceScope = app.Services.GetService<IServiceScopeFactory>().Create
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseHttpsRedirection();
     app.UseMigrationsEndPoint();
 }
 else
 {
+    app.Urls.Add("http://0.0.0.0:8888");
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseHttpsRedirection();
-
 
 app.UseAntiforgery();
 
