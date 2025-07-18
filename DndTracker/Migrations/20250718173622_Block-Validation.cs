4734252f704ab53ea4d2b0efa286ff0edf82d66c
jimmyjawnz <jamesprovee@gmail.com>
2025-07-18 13:56:02 -0600

    Attempting to add validation to encounter block presets

---
+++ DndTracker/Migrations/20250718173622_Block-Validation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DndTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlockValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "display_name",
+                table: "EncounterBlockPresets",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "display_name",
+                table: "EncounterBlockPresets",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+        }
+    }
+}
---
+++ DndTracker/Migrations/20250718182701_Revert-BlockValidation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DndTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class RevertBlockValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "display_name",
+                table: "EncounterBlockPresets",
+                type: "nvarchar(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "display_name",
+                table: "EncounterBlockPresets",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldNullable: true);
+        }
+    }
+}
